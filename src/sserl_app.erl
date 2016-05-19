5f38314c9ad71f1cced84ac0d1b2dfeaf6d62c3c
paul <qianlong.zhou@gmail.com>
2016-05-19 23:50:04 +0800

    feature env support

--- src/sserl_app.erl
+++ src/sserl_app.erl
@@ -15,7 +15,13 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
-    sserl_sup:start_link().
+    case sserl_sup:start_link() of
+        {ok, Pid} ->
+            start_listeners(),
+            {ok, Pid};
+        Error ->
+            Error
+    end.
 
 %%--------------------------------------------------------------------
 stop(_State) ->
@@ -24,3 +30,17 @@ stop(_State) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
+start_listeners() ->
+    {ok, App} = application:get_application(),
+    Listeners = application:get_env(App, listeners, []),
+    lists:map(fun({Name, Args}) ->
+                      case sserl_listener_sup:start(Args) of
+                          {ok, Pid} ->
+                              error_logger:info_msg("~p start listener ~p ok (pid:~p)", [?MODULE, Name, Pid]),
+                              ok;
+                          Error ->
+                              error_logger:error_msg("~p start listener ~p error:~p", [?MODULE, Name, Error]),
+                              throw({error, Error})
+                      end
+              end, Listeners),
+    ok.
